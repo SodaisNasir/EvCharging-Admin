c36ec008d906382dbf83a25acd48c37475cd4871
Mateen Ahmed <107365785+MateenAhmed737@users.noreply.github.com>
2025-04-11 00:22:00 -0700

    Update base_url

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,13 +1,13 @@
-export const base_url = "https://api.charznet.in";
+export const base_url = "https://api.charznet.com";
 export const image_base_url =
-  "https://api.charznet.in/uploads/users/";
+  "https://api.charznet.com/uploads/users/";
 export const users_image_base_url =
-  "https://api.charznet.in/uploads/users/";
+  "https://api.charznet.com/uploads/users/";
 export const station_image_base_url =
-  "https://api.charznet.in/uploads/station_images/";
+  "https://api.charznet.com/uploads/station_images/";
 export const ports_image_base_url =
-  "https://api.charznet.in/uploads/port_images/";
+  "https://api.charznet.com/uploads/port_images/";
 export const country_image_base_url =
-  "https://api.charznet.in/uploads/country_images/";
+  "https://api.charznet.com/uploads/country_images/";
 export const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NTg4MTAyMjFkYWU3N2Nk";
